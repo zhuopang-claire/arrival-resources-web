9fdad996960390e84c569dbe69db0556c686602f
Claire Zhuo Pang <45908999+zhuopang-claire@users.noreply.github.com>
2026-03-08 20:10:39 -0400

    add vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
@@ -33,7 +34,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="app-shell">
           <Header />
-          <main className="app-main">{children}</main>
+          <main className="app-main">{children}
+          <SpeedInsights />
+          </main>
           <Footer />
         </div>
       </body>
